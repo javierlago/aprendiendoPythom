15c3e9306bede350d65e0a762ce1189d59bfb609
Javier Lago Amoedo <javierlagoamoedo.daw@gmail.com>
2023-09-16 11:39:25 +0200

    mi primer ejercicio

---
+++ 00_helloWorld.py
@@ -0,0 +1,17 @@
+#Esto en un comentario en una linea
+"""Esto es un
+comentario en varias
+lineas"""
+print("Hola mundo")
+'''Los comentarios tambien
+se pueden usar con comillas simples'''
+print('--------------')
+print('Hola mundo')
+"""Los print tambien se pueden hacer con comillas simples"""
+print('--------------')
+print(type('Hola mundo'))
+print(type(5))
+print(type(1.2))
+print(type(True))
+"""Con el 'Type' te printea el tipo de dato"""
+
---
+++ 01_variables.py
@@ -0,0 +1,17 @@
+#Variables
+
+my_variable="My string variable"
+print(my_variable);
+
+my_int_variable= 5;
+print(my_int_variable);
+
+my_bool_variable = True
+print(my_bool_variable)
+
+#Concatenacion de variables en un print.
+print(my_bool_variable,my_variable,my_int_variable)
+print(len(my_variable)) #Te dice el largo de la cadena
+
+#Variable en una sola linea
+
