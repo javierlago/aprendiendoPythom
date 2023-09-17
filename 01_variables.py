f2af4d4af1991d55bc98ef273623a84c5862cc6a
Javier Lago Amoedo <javierlagoamoedo.daw@gmail.com>
2023-09-17 11:05:09 +0200

    wp

--- 01_variables.py
+++ 01_variables.py
@@ -1,18 +1,26 @@
-#Variables
+# Variables #
 
-my_variable="My string variable"
-print(my_variable);
+my_variable = "My string variable"
+print(my_variable)
 
-my_int_variable= 5;
-print(my_int_variable);
+my_int_variable = 5
+print(my_int_variable)
 
 my_bool_variable = True
 print(my_bool_variable)
 
-#Concatenacion de variables en un print.
-print(my_bool_variable,my_variable,my_int_variable)
-print(len(my_variable)) #Te dice el largo de la cadena
+# Concatenacion de variables en un print. #
+print(my_bool_variable, my_variable, my_int_variable)
+print(len(my_variable))   # Te dice el largo de la cadena #
+print("Este es el valor de:", my_bool_variable)
 
-#Variable en una sola linea
+# Variable en una sola linea #
+name, surname, alias, age = "Javier", "Lago", "Reibax", 38
 
+print('Mi nombre es ', name, 'y me apellido', surname)
 
+# Entrada de datos por teclado #
+name = input('Como te llamas')
+age = input('Cuantos años tienes')
+
+print('Me llamo ', name, 'y mi edad es de ', age)
